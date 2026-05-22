36ee007c9870f997f1d9be82866e2e911f4121ea
LeonTan <leotan@student.42kl.edu.my>
2026-05-22 13:34:27 +0800

    feat: prompt_model added "smart" model selection with match case and default case for errors

--- prompt_mode.py
+++ prompt_mode.py
@@ -1,3 +1,17 @@
 def prompt_model(model: str, prompt: str) -> str :
-	# Your code here...
-	pass
+	match model:
+		case "llama3.1":
+			pass
+		case "phi3":
+			pass
+		case "deepseek-r1":
+			pass
+		case "gemini-2.5-flash":
+			pass
+		case "gemini-2.5-flash-lite":
+			pass
+		case "gemini-3-flash-preview":
+			pass
+		case _:
+			raise ValueError(f"Error: model not found \"{model}\"")
+	return
