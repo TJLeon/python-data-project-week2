f18adc2ac889a661698a67624b3b70041cbb70c3
LeonTan <leotan@student.42kl.edu.my>
2026-05-23 15:18:05 +0800

    feat: main function calls prompt_model with selected ai model and prompt message, prompt_model runs supported ai models and return response including error handling

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
-def main():
-    print("Hello from week2!")
+import sys
+from prompt_model import prompt_model
 
+def main():
+	argc = len(sys.argv)
+
+	if argc == 1:
+		print(f"{prompt_model("tinyllama", "introduce yourself")}")
+		return
+
+	if argc != 3:
+		print("Usage: uv run main.py <model> <prompt>")
+		return
+
+	model = sys.argv[1]
+	prompt = sys.argv[2]
+
+	result = prompt_model(model, prompt)
+	print(result)
 
 if __name__ == "__main__":
-    main()
+	main()
--- prompt_model.py
+++ prompt_model.py
@@ -1,25 +1,32 @@
 from ollama import generate
+from dotenv import load_dotenv
 from google import genai
+
 
 def ollama_model(ai_model, msg):
 	try:
 		res = generate(model=ai_model, prompt=msg)
+		return res['response']
 	except Exception as e:
-		print(f"Error: {ai_model} {e}")
+		print(f"Error: \"{ai_model}\" {e}")
 		print(f"Input: \"{msg}\"")
+		return ""
 
 def gemini_model(ai_model, msg):
 	try:
+		load_dotenv()
 		client = genai.Client()
 
 		res = client.models.generate_content(model=ai_model, contents=msg)
+		return res.text
 	except Exception as e:
-		print(f"Error: {ai_model} {e}")
+		print(f"Error: \"{ai_model}\" {e}")
 		print(f"Input: \"{msg}\"")
+		return ""
 
 def prompt_model(model: str, prompt: str) -> str :
 	match model:
-		case "llama3.1"| "phi3"| "deepseek-r1":
+		case "llama3.1"| "phi3"| "deepseek-r1" | "tinyllama":
 			return ollama_model(model, prompt)
 		case "gemini-2.5-flash" | "gemini-2.5-flash-lite" | "gemini-3-flash-preview":
 			return gemini_model(model, prompt)
