24dfef89c837a8849ee243debb7e11bcfe144456
LeonTan <leotan@student.42kl.edu.my>
2026-05-23 12:54:58 +0800

    refactor: bundling match case cases together

--- prompt_mode.py
+++ prompt_mode.py
@@ -1,21 +1,28 @@
-from ollama import chat
-from ollama import ChatResponse
+from ollama import generate
 from google import genai
+
+def ollama_model(ai_model, msg):
+	try:
+		res = generate(model=ai_model, prompt=msg)
+	except Exception as e:
+		print(f"Error: {ai_model} {e}")
+		print(f"Input: \"{msg}\"")
+
+def gemini_model(ai_model, msg):
+	try:
+		client = genai.Client()
+
+		res = client.models.generate_content(model=ai_model, contents=msg)
+	except Exception as e:
+		print(f"Error: {ai_model} {e}")
+		print(f"Input: \"{msg}\"")
 
 def prompt_model(model: str, prompt: str) -> str :
 	match model:
-		case "llama3.1":
+		case "llama3.1"| "phi3"| "deepseek-r1":
 			pass
-		case "phi3":
-			pass
-		case "deepseek-r1":
-			pass
-		case "gemini-2.5-flash":
-			pass
-		case "gemini-2.5-flash-lite":
-			pass
-		case "gemini-3-flash-preview":
+		case "gemini-2.5-flash" | "gemini-2.5-flash-lite" | "gemini-3-flash-preview":
 			pass
 		case _:
-			raise ValueError(f"Error: model not found \"{model}\"")
+			return(f"Error: model not found \"{model}\"")
 	return
